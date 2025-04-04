4259c3d8431a588f4a1e4bb9a20b1177e3069ab4
Ram <ram.dubey@scalecapacity.com>
2025-04-05 00:40:23 +0530

    Removed Single workflow

--- lib/cdk-poc-stack.ts
+++ lib/cdk-poc-stack.ts
@@ -6,7 +6,7 @@ export class CdkPocStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here   
+    // The code that defines your stack goes here   21654
     
     // const queue = new sqs.Queue(this, 'CdkPocQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
