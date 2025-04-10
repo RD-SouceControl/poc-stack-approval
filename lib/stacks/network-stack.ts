b62cc30752f3eb344f6934a7e9a69bceb3696a0f
Ram <ram.dubey@scalecapacity.com>
2025-04-10 18:41:08 +0530

    Output Check 0.1

--- lib/stacks/network-stack.ts
+++ lib/stacks/network-stack.ts
@@ -9,8 +9,9 @@ export class NetworkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Original VPC (unchanged)
+    // Primary VPC with explicit CIDR
     this.vpc = new ec2.Vpc(this, 'Vpc', {
+      cidr: '10.0.0.0/16',
       maxAzs: 2,
       natGateways: 0,
     });
@@ -19,7 +20,13 @@ export class NetworkStack extends cdk.Stack {
       value: this.vpc.vpcId,
     });
 
+    new cdk.CfnOutput(this, 'VpcCidr', {
+      value: this.vpc.vpcCidrBlock,
+    });
+
+    // Extra VPC with explicit CIDR
     const extraVpc = new ec2.Vpc(this, 'ExtraVpc', {
+      cidr: '10.1.0.0/16',
       maxAzs: 2,
       natGateways: 1,
       subnetConfiguration: [
@@ -36,7 +43,7 @@ export class NetworkStack extends cdk.Stack {
       ],
     });
 
-    // // ALB in the public subnet
+    // ALB in the public subnet
     const alb = new elbv2.ApplicationLoadBalancer(this, 'ExtraALB', {
       vpc: extraVpc,
       internetFacing: true,
@@ -45,6 +52,10 @@ export class NetworkStack extends cdk.Stack {
 
     new cdk.CfnOutput(this, 'ExtraVpcId', {
       value: extraVpc.vpcId,
+    });
+
+    new cdk.CfnOutput(this, 'ExtraVpcCidr', {
+      value: extraVpc.vpcCidrBlock,
     });
 
     new cdk.CfnOutput(this, 'ExtraALBDNS', {
