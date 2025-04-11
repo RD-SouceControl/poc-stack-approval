abfe7e8b1732ab6b8fb32966dc0d44357cb0d64f
Ram <ram.dubey@scalecapacity.com>
2025-04-12 02:02:40 +0530

    New Format 0.6

--- lib/stacks/network-stack.ts
+++ lib/stacks/network-stack.ts
@@ -57,9 +57,9 @@ export class NetworkStack extends cdk.Stack {
       allowAllOutbound: true,
     });
 
-    albSg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP');
-    albSg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), 'Allow HTTPS');
-    albSg.addEgressRule(ec2.Peer.anyIpv4(), ec2.Port.allTraffic(), 'Allow all egress');
+    // albSg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP');
+    // albSg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), 'Allow HTTPS');
+    // albSg.addEgressRule(ec2.Peer.anyIpv4(), ec2.Port.allTraffic(), 'Allow all egress');
 
     cdk.Tags.of(albSg).add('Name', 'ExtraALBSG');
 
